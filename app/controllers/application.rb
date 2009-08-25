334076c2c4a4d1204f35af59fe00d3c43ad88e1a
Stuart Halloway <stu@thinkrelevance.com>
2009-08-24 22:19:13 -0400

    ajax demos, more utility examples

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => 'cb478af098cfa9f3152ab0a6f6c4dbad'
+  # protect_from_forgery # :secret => 'cb478af098cfa9f3152ab0a6f6c4dbad'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -1,2 +1,6 @@
 class DemosController < ApplicationController
+  def echo
+    @args = params.delete_if {|k,v| k == "action" || k == "controller" }
+    render :layout => false
+  end
 end
