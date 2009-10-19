800c63432ac8dc023a20c536ad7546ee111bfed2
Stuart Halloway <stu@thinkrelevance.com>
2009-10-19 17:39:08 -0400

    more demos

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,40 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+function formatObject(result) {
+    var formatted = "";
+    jQuery.each(result, function(name, val) {
+        formatted = formatted + name + ": " + val + "\n   ";
+    })
+    return formatted;
+}
+
+function formatArray(result) {
+    return "[" + result + "]";
+}
+
+function formatFunction(result) {
+    return String(result).match(/(function[^{]+)/)[0] + "{ ... }";
+}
+
+function formatResult(result) {
+    if (result == null) {
+        return null;
+    }
+    if (jQuery.isFunction(result)) {
+        return formatFunction(result);
+    }
+    if (jQuery.isArray(result)) {
+        return formatArray(result);
+    }
+    if (typeof(result) == "object") {
+        return formatObject(result);
+    }
+    return result;   
+}
+
+function evalCode() {
+  try {
+    return eval("(" + jQuery("#code").val() + ")");
+  } catch (err) {
+    return err.message;
+  }
+}
+
